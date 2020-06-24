5b2c4b4417b49d74b6312f2ee52c7154966b7c44
[rryanburton] <ryan.burton@da.org>
2020-06-24 09:53:42 -0400

    fixed timing

--- control.py
+++ control.py
@@ -3,10 +3,11 @@ import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)  # GPIO Numbers instead of board numbers
-
 RELAY_1_GPIO = 23
-GPIO.setup(RELAY_1_GPIO, GPIO.OUT)  # GPIO Assign mode
-GPIO.output(RELAY_1_GPIO, GPIO.LOW)  # out
-GPIO.output(RELAY_1_GPIO, GPIO.HIGH)  # on
-time.sleep(0.5)
-GPIO.output(RELAY_1_GPIO, GPIO.LOW)  # on
+while True:
+    GPIO.setup(RELAY_1_GPIO, GPIO.OUT)  # GPIO Assign mode
+    GPIO.output(RELAY_1_GPIO, GPIO.LOW)  # turns off
+    GPIO.output(RELAY_1_GPIO, GPIO.HIGH)  # turns on
+    time.sleep(2)                         # wait 2 sec
+    GPIO.output(RELAY_1_GPIO, GPIO.LOW)  # turns off
+    time.sleep(20)                      # wait 20 sec
