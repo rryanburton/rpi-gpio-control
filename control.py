489c44fe26f9ba17c9b32b5c6b8a7be1de0e01f5
[rryanburton] <ryan.burton@da.org>
2020-06-24 09:34:04 -0400

    initial commit

---
+++ control.py
@@ -0,0 +1,8 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)  # GPIO Numbers instead of board numbers
+
+RELAY_1_GPIO = 23
+GPIO.setup(RELAY_1_GPIO, GPIO.OUT)  # GPIO Assign mode
+GPIO.output(RELAY_1_GPIO, GPIO.LOW)  # out
+GPIO.output(RELAY_1_GPIO, GPIO.HIGH)  # on
