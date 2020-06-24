306e322db412b6270efbb5e1f5b5e0dd2fb8e755
[rryanburton] <ryan.burton@da.org>
2020-06-24 09:43:15 -0400

    more

--- control.py
+++ control.py
@@ -1,3 +1,5 @@
+import time
+
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)  # GPIO Numbers instead of board numbers
@@ -6,3 +8,5 @@ RELAY_1_GPIO = 23
 GPIO.setup(RELAY_1_GPIO, GPIO.OUT)  # GPIO Assign mode
 GPIO.output(RELAY_1_GPIO, GPIO.LOW)  # out
 GPIO.output(RELAY_1_GPIO, GPIO.HIGH)  # on
+time.sleep(0.5)
+GPIO.output(RELAY_1_GPIO, GPIO.LOW)  # on
